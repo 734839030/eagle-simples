c6a4464fef986924e5ed28bbbcfc3f3c72d1f6c7
734839030 <734839030@qq.com>
2017-11-19 21:48:28 +0800

    add rocket mq 事务消息

--- eagle-rocketmq-demo/src/main/java/com/seezoon/eagle/rocketmq/demo/consumer/DemoOrderlyMessageListener.java
+++ eagle-rocketmq-demo/src/main/java/com/seezoon/eagle/rocketmq/demo/consumer/DemoOrderlyMessageListener.java
@@ -6,7 +6,12 @@ import org.apache.rocketmq.common.message.MessageExt;
 import org.springframework.stereotype.Component;
 
 import com.seezoon.eagle.rocketmq.listener.AbstractMessageListenerOrderly;
-
+/**
+ * 更多demo 参见官方
+ * https://github.com/apache/rocketmq/tree/master/example/src/main/java/org/apache/rocketmq/example
+ * @author hdf
+ * 2017年11月18日
+ */
 @Component
 public class DemoOrderlyMessageListener extends AbstractMessageListenerOrderly {
 
--- eagle-rocketmq-demo/src/test/java/com/seezoon/eagle/rocketmq/demo/TestDefaultMqProducer.java
+++ eagle-rocketmq-demo/src/test/java/com/seezoon/eagle/rocketmq/demo/TestDefaultMqProducer.java
@@ -1,16 +1,28 @@
 package com.seezoon.eagle.rocketmq.demo;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.MessageQueueSelector;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.producer.TransactionCheckListener;
+import org.apache.rocketmq.client.producer.TransactionMQProducer;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alibaba.fastjson.JSON;
 import com.seezoon.eagle.core.test.BaseJunitTest;
 import com.seezoon.eagle.core.utils.NDCUtils;
+import com.seezoon.eagle.rocketmq.demo.transaction.TransactionCheckListenerImpl;
+import com.seezoon.eagle.rocketmq.demo.transaction.TransactionExecuterImpl;
 
 public class TestDefaultMqProducer extends BaseJunitTest{
 
@@ -100,5 +112,52 @@ public class TestDefaultMqProducer extends BaseJunitTest{
 		} catch (Exception e) {
 			e.printStackTrace();
 		} 
+	}
+	/**
+	 * 批量发送4.1 + 版本
+	 * @throws MQClientException
+	 * @throws RemotingException
+	 * @throws MQBrokerException
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void sendBatch() throws MQClientException, RemotingException, MQBrokerException, InterruptedException {
+		String topic = "BatchTest";
+        List<Message> messages = new ArrayList<>();
+        messages.add(new Message(topic, "Tag", "OrderID001", "Hello world 0".getBytes()));
+        messages.add(new Message(topic, "Tag", "OrderID002", "Hello world 1".getBytes()));
+        messages.add(new Message(topic, "Tag", "OrderID003", "Hello world 2".getBytes()));
+        SendResult send = defaultMQProducer.send(messages);
+	}
+	@Test
+	public void transactionMsg() throws Exception {
+		TransactionCheckListener transactionCheckListener = new TransactionCheckListenerImpl();
+        TransactionMQProducer producer = new TransactionMQProducer("please_rename_unique_group_name");
+        producer.setNamesrvAddr("192.168.221.133:9876");
+        producer.setCheckThreadPoolMinSize(2);
+        producer.setCheckThreadPoolMaxSize(2);
+        producer.setCheckRequestHoldMax(2000);
+        producer.setTransactionCheckListener(transactionCheckListener);
+        producer.start();
+        String[] tags = new String[] {"TagA", "TagB", "TagC", "TagD", "TagE"};
+        TransactionExecuterImpl tranExecuter = new TransactionExecuterImpl();
+        for (int i = 0; i < 100; i++) {
+            try {
+                Message msg =
+                    new Message("TopicTest1", tags[i % tags.length], "KEY" + i,
+                        ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                SendResult sendResult = producer.sendMessageInTransaction(msg, tranExecuter, null);
+                System.out.printf("%s%n", sendResult);
+
+                Thread.sleep(1000);
+            } catch (MQClientException | UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (int i = 0; i < 100000; i++) {
+            Thread.sleep(1000);
+        }
+        producer.shutdown();
 	}
 }
